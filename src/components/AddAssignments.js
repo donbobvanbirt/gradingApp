85e3044b7e7a78b4cdb4756709c1c568de659f5c
Donovan <donovan6925@gmail.com>
2016-09-26 09:43:50 -0700

    initial commit

---
+++ src/components/AddAssignments.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const AddAssignments = props => {
+
+  return (
+    <div>
+      <h1>AddAssignments</h1>
+    </div>
+  )
+}
+
+export default AddAssignments;
